e03b39ee927a60078a0d6570bc191c1e2548f5ea
Sutantu Balamurugan <sutan04@gmail.com>
2025-06-24 09:45:27 -0700

    Statistics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
 	import '../app.css';
-	
+	import { injectAnalytics } from '@vercel/analytics/sveltekit'
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+	import { onMount } from 'svelte';
+
 	let { children } = $props();
+
+	onMount(() => {
+		injectAnalytics();
+		injectSpeedInsights();
+	});
 </script>
 
 {@render children()}
